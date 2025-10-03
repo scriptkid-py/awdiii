48814ebcf804cd63b4f16060f9c0a6fc58648dc1
scriptkid-py <r44chr@gmail.com>
2025-10-03 21:55:02 +0100

    Add Instagram link for Zeyd in About Us page

--- AboutUs.tsx
+++ AboutUs.tsx
@@ -67,6 +67,10 @@ const AboutUs: React.FC = () => {
               <p className="about-us__role">Back-end Developer</p>
               <p className="about-us__bio">who needs bio when I'm just up</p>
               <nav className="about-us__social" aria-label="Social links">
+                <a className="about-us__social-link" href="https://www.instagram.com/xzaid_endx/" target="_blank" rel="noopener noreferrer" aria-label="Follow on Instagram">
+                  <span className="visually-hidden">Instagram</span>
+                  <svg className="about-us__icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path fill="currentColor" d="M7 2C4.2 2 2 4.2 2 7v10c0 2.8 2.2 5 5 5h10c2.8 0 5-2.2 5-5V7c0-2.8-2.2-5-5-5H7zm10 2c1.7 0 3 1.3 3 3v10c0 1.7-1.3 3-3 3H7c-1.7 0-3-1.3-3-3V7c0-1.7 1.3-3 3-3h10zm-5 3.5A5.5 5.5 0 1017.5 13 5.51 5.51 0 0012 7.5zm0 2A3.5 3.5 0 1115.5 13 3.5 3.5 0 0112 9.5zm5.8-3.5a1 1 0 10.2 2 1 1 0 00-.2-2z"/></svg>
+                </a>
                 <a className="about-us__social-link" href="mailto:ammizaidghost@gmail.com" aria-label="Email Zeyd">
                   <span className="visually-hidden">Email</span>
                   <svg className="about-us__icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path fill="currentColor" d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
